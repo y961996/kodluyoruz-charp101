2b82dbb270d02911036d47efb17e22569d270972
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-28 23:00:07 +0300

    donguler-while-foreach tamamlandi.

---
+++ Pratikler/donguler-while-foreach/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Bir sayı gir gardaş:");
+            int sayı = int.Parse(Console.ReadLine());
+            int sayaç = 1;
+            int sum = 0;
+            while(sayaç <= sayı)
+            {
+                sum += sayaç;
+                sayaç++;
+            }
+
+            Console.WriteLine(sum / sayı);
+            */
+
+            char c = 'a';
+            while(c <= 'z')
+            {
+                Console.Write(c);
+                c++;
+            }
+
+            Console.WriteLine();
+            string[] cars = {"asd", "asdsada", "wetsdf", "gsd"};
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
