a65e0a32e9470f6899cd497c99c023ea21b9580b
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-28 22:37:40 +0300

    hata-yonetimi-2 tamamlandi.

---
+++ Pratikler/hata-yonetimi-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hata_yonetimi_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayı = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı: " + sayı);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Hata: " + e.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
