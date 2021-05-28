c466aee428e6c7eab7b745372578089272dce497
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-28 16:12:24 +0300

    Ilk pratik tamamlandi.

---
+++ Pratikler/console-programlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("İsminizi Giriniz: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyisminizi Giriniz:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
