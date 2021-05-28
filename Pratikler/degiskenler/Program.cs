506f8bc204582e2db7d5d09faee0d4348858ab25
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-28 16:40:53 +0300

    Pratikler/degiskenler tamamlandi.

--- Pratikler/degiskenler/Program.cs
+++ Pratikler/degiskenler/Program.cs
@@ -36,6 +36,37 @@ namespace degiskenler
             */
             DateTime dt = DateTime.Now;
             Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2 = 't';
+            object o3 = 72;
+            object o4 = 7.2;
+
+            // string ifadeler
+            string str1 = string.Empty; // string str1 = null;
+            str1 = "GuciGenk";
+            string ad = "Guci";
+            string soyad = "Genk";
+            string tamİsim = ad + " " + soyad;
+
+            // Değişken dönüşümleri
+            string str72 = "72";
+            int int72 = 72;
+            string newVal = str72 + int72.ToString();
+
+            Console.WriteLine(newVal);
+
+            int testVal = int72 + Convert.ToInt32("3");
+            Console.WriteLine(testVal);
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(datetime2);
+
+            string hourtime = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hourtime);
         }
     }
 }
