bbf543ffd18b92c72aa89270be2ea7387e29b8b6
Yunus Atahan <yunus10atahan@gmail.com>
2021-05-28 18:53:27 +0300

    operatorler tamamlandi.

--- Pratikler/operatorler/Program.cs
+++ Pratikler/operatorler/Program.cs
@@ -6,7 +6,36 @@ namespace operatorler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 3;
+            int y = 3;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y *= 4;
+            Console.WriteLine(y);
+            y /= 3;
+            Console.WriteLine(y);
+            x -= 2;
+            Console.WriteLine(x);
+
+            // Matnıksal Operatorler
+            bool isGuci = false;
+            bool isGenk = true;
+            bool isGuciGenk = isGuci && isGenk;
+            Console.WriteLine(isGuciGenk);
+
+            int small = 0;
+            int big = 100;
+
+            if(small < big) Console.WriteLine(small < big);
+
+            small += 100;
+            big -= 100;
+
+            if(small < big) Console.WriteLine(small < big);
         }
     }
 }
