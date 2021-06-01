3df2e0ff6545053d80330bd002523baca6fb4aef
Yunus Atahan <yunus10atahan@gmail.com>
2021-06-01 16:39:26 +0300

    rehberden kisi silme eklendi

--- Odevler/Proje-1/TelefonRehberi.cs
+++ Odevler/Proje-1/TelefonRehberi.cs
@@ -40,17 +40,31 @@ namespace Proje_1
       string input = Console.ReadLine();
       if(IsimVeyaSoyisimeGoreAra(input)){
         Console.WriteLine(input + " isimli kişi rehberden silinmek üzere, onaylıyor musunuz ?(y/n)");
+        string opt = Console.ReadLine();
+        if(opt.Equals("y")){
+          for(int i = 0; i < this.rehber.Count; i++){
+            if(this.rehber[i].Isim.Equals(input) || this.rehber[i].Soyisim.Equals(input)){
+              this.rehber.RemoveAt(i);
+            }
+          }
+        }
       } 
       else
       {
         Console.WriteLine("Aradığınız krtiterlere uygun veri rehberde bulunamadı. Lütfen bir seçim yapınız.");
         Console.WriteLine("* Silmeyi sonlandırmak için : (1)");
         Console.WriteLine("* Yeniden denemek için      : (2)");
+        string scnk = Console.ReadLine();
+        if(scnk.Equals("1")) return;
+        else if(scnk.Equals("2")) RehberdenKişiSil();
+        else Console.WriteLine("Yanlis secim !");
       }
     }
 
     private bool IsimVeyaSoyisimeGoreAra(string s){
-      this.rehber.ForEach(kisi => Console.WriteLine(kisi.Isim));
+      for(int i = 0; i < this.rehber.Count; i++){
+        if(this.rehber[i].Isim.Equals(s) || this.rehber[i].Soyisim.Equals(s)) return true;
+      }
       return false;
     }
 
