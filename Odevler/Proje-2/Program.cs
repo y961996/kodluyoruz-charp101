673ad0aaefde0ac1af9960457617cccaa7ddb9fc
Yunus Atahan <yunus10atahan@gmail.com>
2021-06-01 23:07:08 +0300

    main loop eklendi

--- Odevler/Proje-2/Program.cs
+++ Odevler/Proje-2/Program.cs
@@ -16,6 +16,29 @@ namespace Proje_2
 
             Board board = new Board();
 
+            int option;
+            do
+            {
+                PrintMenu();
+                option = int.Parse(Console.ReadLine());
+                handleOption(option);
+            } while (option > 0 && option <4);
+
+            Console.WriteLine("Geçersiz seçim! Programdan çıkılıyor...");
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :)");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("(1) Board Listelemek");
+            Console.WriteLine("(2) Board'a Kart Eklemek");
+            Console.WriteLine("(3) Board'dan Kart Silmek");
+            Console.WriteLine("(4) Kart Taşımak");
+        }
+
+        public static void handleOption(int option)
+        {
             
         }
     }
