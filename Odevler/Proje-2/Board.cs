99606fe3df59199f1c319afc320be58596538cfc
Yunus Atahan <yunus10atahan@gmail.com>
2021-06-02 12:12:43 +0300

    takimdaki kisiler metodu yerine bir degisken tanimlandi

--- Odevler/Proje-2/Board.cs
+++ Odevler/Proje-2/Board.cs
@@ -8,32 +8,28 @@ namespace Proje_2
     private Line inProgress;
     private Line done;
 
+    public Kişi[] takımdakiKişiler = {
+      new Kişi(0, "Ümit Tosun"),
+      new Kişi(1, "Tevfik Sokullu"),
+      new Kişi(2, "Burçin Özoguz"),
+      new Kişi(3, "Sabah Gülek"),
+      new Kişi(4, "Esen Akbulut")
+    };
+
     public Line Todo { get => todo; set => todo = value; }
     public Line InProgress { get => inProgress; set => inProgress = value; }
     public Line Done { get => done; set => done = value; }
 
     public Board()
     {
-      Kart defaultKart1 = new Kart("Başlık1", "İçerik çok dolu değil.", kişiler()[0], KartBüyüklük.S);
-      Kart defaultKart2 = new Kart("Başlık2", "İçerik çok dolu.", kişiler()[1], KartBüyüklük.L);  
-      Kart defaultKart3 = new Kart("Başlık3", "İçerik boş.", kişiler()[3], KartBüyüklük.XS);
+      Kart defaultKart1 = new Kart("Başlık1", "İçerik çok dolu değil.", takımdakiKişiler[0], KartBüyüklük.S);
+      Kart defaultKart2 = new Kart("Başlık2", "İçerik çok dolu.", takımdakiKişiler[1], KartBüyüklük.L);  
+      Kart defaultKart3 = new Kart("Başlık3", "İçerik boş.", takımdakiKişiler[3], KartBüyüklük.XS);
 
       todo.Kartlar.Add(defaultKart1);
       todo.Kartlar.Add(defaultKart3);
 
       InProgress.Kartlar.Add(defaultKart2);
-    }
-
-    internal Kişi[] kişiler()
-    {
-      Kişi[] takımdakiKişiler = {
-        new Kişi(0, "Ümit Tosun"),
-        new Kişi(1, "Tevfik Sokullu"),
-        new Kişi(2, "Burçin Özoguz"),
-        new Kişi(3, "Sabah Gülek"),
-        new Kişi(4, "Esen Akbulut")
-      };
-      return takımdakiKişiler;
     }
     internal void BoardListele()
     {
